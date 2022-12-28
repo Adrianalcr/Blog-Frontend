cae6965a84cdce0499633a24725e5a44631244ad
Adriana Lima <adrianalima-dev@outlook.com>
2022-12-28 15:03:23 -0300

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const Footer = () => {
+  //Button Scroll Up
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
   return (
-    <div className="main-header">
-        Footer
+    <div className="footer">
+      <div className="navbar">
+        <a href="https://adrianalima.vercel.app" target="_blank">Developer: Adriana Lima</a>
+
+        <div onClick={scrollToTop}>
+          <img
+            src="/scroll-up-50.png"
+            className="scroollUp"
+            alt="Volte ao topo"
+            priority
+          />
+        </div>
+
+      </div>
     </div>
   )
 }
